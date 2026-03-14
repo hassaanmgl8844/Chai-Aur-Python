36b241aad48664c20fff762677c90fa8f8c06da1
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-03-14 13:50:08 +0500

    Before video of Data Types

---
+++ 01_Basics/chai.py
@@ -0,0 +1,5 @@
+from hello_chai import hello_chai
+
+hello_chai("Bye Billu")
+
+# the quick brown fox jumps over the lazy dog.
---
+++ 01_Basics/hello_chai.py
@@ -0,0 +1,10 @@
+print("Billu Jutt")
+
+def hello_chai (n):
+    print(n)
+
+hello_chai("Hello Billu")
+
+chai_one = "lemon tea"
+chai_two = "Ginger Tea"
+chai_three = "Masala Chai"
