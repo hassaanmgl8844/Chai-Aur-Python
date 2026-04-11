a1fbc194268503d6ff15bdc3969a1bb7aa5507fa
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-04-11 13:29:49 +0500

    First Problem Solved

---
+++ 02_Conditionals/01_Solution.py
@@ -0,0 +1,12 @@
+# Age Group Categorization
+
+age = int(input("Enter Your Age: "))
+
+if (age < 13):
+    print("You're a Child.")
+elif (age < 19):
+    print("You're a Teenager.")
+elif (age < 60):
+    print("You're a Adult.")
+else:
+    print("You're Senior.")
