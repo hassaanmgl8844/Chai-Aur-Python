a11cd7c6b883d945ffbc4ad7f0fc06857eb398fc
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-04-11 13:50:15 +0500

    2nd Mistake Solved By Me.

---
+++ 02_Conditionals/02_Solution.py
@@ -0,0 +1,9 @@
+# Movie Ticked Pricing
+age = int(input("Enter Your Fucking Age MF: "))
+day = input("Enter The day Today: ")
+
+price = 12 if age >= 18 else 8
+if day == "Wednesday":
+    price = price -2
+
+print("Your Price Is $",price) 
