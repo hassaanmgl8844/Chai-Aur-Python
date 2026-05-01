1c4759121fc1cd956a3844e170ee5e15a1060fd3
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-01 13:39:41 +0500

    Done

--- Different.py
+++ Different.py
@@ -3,3 +3,15 @@
 #     print("You're Adult")
 # else:
 #     print("You're a Minor")
+
+# Ask user to enter a number
+# If number is positive → print "Positive"
+# If number is negative → print "Negative"
+# If number is zero → print "Zero"
+user = int(input("Enter a Number: "))
+if user % 2 == 0:
+    print("This is Postive Number.")
+elif user % 3 == 0:
+    print("This is Negative Number.")
+else:
+    print("You're Printing Zero Baby!")
