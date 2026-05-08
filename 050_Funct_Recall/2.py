a5d7fb3ec178bba1825230101a5f1b7b40e41b35
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-08 22:16:31 +0500

    2nd Problem Solved

---
+++ 050_Funct_Recall/2.py
@@ -0,0 +1,10 @@
+# ✏️ Write a function called is_even that takes a number and returns True if it's even, False if it's odd.
+# is_even(4)  → True
+# is_even(7)  → False
+
+def is_even(num): 
+    if num % 2 == 0: 
+        return True
+    else:
+        return False
+is_even(5)
