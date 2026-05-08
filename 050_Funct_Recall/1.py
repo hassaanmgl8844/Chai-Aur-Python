133f6afbb1258a2f21919c1ccbfbd6d022106126
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-08 22:09:56 +0500

    1st Problem Solved

---
+++ 050_Funct_Recall/1.py
@@ -0,0 +1,6 @@
+# ✏️ Problem 1: Write a function called square that takes a number and returns its square (number × number).
+# Example: square(4) → should give 16
+
+def square (num):
+    return num ** 2
+print(square(5))
