f19df43b2dc178cce1d38b3e7e3b49e3bf942d48
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-09 12:44:03 +0500

    Bad Practice Done!

--- 06_Scopes/01_Scope.py
+++ 06_Scopes/01_Scope.py
@@ -1,7 +1,15 @@
-x = 99
-def func2(y):
-    z = x + y
-    return z
+# x = 99
+# def func2(y):
+#     z = x + y
+#     return z
 
-result = func2(1)
-print(result)
+# result = func2(1)
+# print(result)
+
+# Bad Practice
+# x = 1122
+# def func3():
+#     global x
+#     x = 12
+# func3()
+# print(x)
