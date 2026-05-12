71df0f58f556fc9abd0284f81ab198f20d8aa61c
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-12 14:08:10 +0500

    New Functionality Added in Class

--- 07_OOP's/01_Solution.py
+++ 07_OOP's/01_Solution.py
@@ -4,9 +4,13 @@ class Car:
         self.brand = brand
         self.model = model
 
+    def full_name (self):
+        return f"{self.brand} {self.model}"
+
 my_car = Car("Toyata","Corolla")
 print(my_car.brand)
 print(my_car.model)
+print(my_car.full_name())
 
 my_new_car = Car("Ford","Mustang")
 print(my_new_car.model)
