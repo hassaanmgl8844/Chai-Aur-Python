36ea0b64cb24cc33e13ca6e5fdf4ad6713a96b71
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-12 14:20:57 +0500

    Inheritence Learned

--- 07_OOP's/01_Solution.py
+++ 07_OOP's/01_Solution.py
@@ -1,16 +1,26 @@
 # Create a Car Class with attributes like brandes and models. Than create an instance of Class.
 class Car:
-    def __init__(self,brand,model):
+    def __init__(self, brand, model):
         self.brand = brand
         self.model = model
 
-    def full_name (self):
+    def full_name(self):
         return f"{self.brand} {self.model}"
 
-my_car = Car("Toyata","Corolla")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
 
-my_new_car = Car("Ford","Mustang")
-print(my_new_car.model)
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+
+my_tesla = ElectricCar("Tesla", "Model S", "1122KWh")
+print(my_tesla.model)
+
+# my_car = Car("Toyata","Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
+# my_new_car = Car("Ford","Mustang")
+# print(my_new_car.model)
