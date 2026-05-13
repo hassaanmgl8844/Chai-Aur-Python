1d812caa8b2f83115da56b06af346b7ce5315d60
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-13 10:06:00 +0500

    It's Done

--- 07_OOP's/01_Solution.py
+++ 07_OOP's/01_Solution.py
@@ -32,3 +32,47 @@ class ElectricCar(Car):
     def fuel_type():
         return "Electric charge"
 
+
+# my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+
+# print(isinstance(my_tesla, Car))
+# print(isinstance(my_tesla, ElectricCar))
+
+# print(my_tesla.__brand)
+# print(my_tesla.fuel_type())
+
+# my_car = Car("Tata", "Safari")
+# my_car.model = "City"
+# Car("Tata", "Nexon")
+
+
+# print(my_car.general_description())
+# print(my_car.model)
+
+
+# my_car = Car("Toyota", "Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
+# my_new_car = Car("Tata", "Safari")
+# print(my_new_car.model)
+
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+
+
+class Engine:
+    def engine_info(self):
+        return "This is engine"
+
+
+class ElectricCarTwo(Battery, Engine, Car):
+    pass
+
+
+my_new_tesla = ElectricCarTwo("Tesla", "Model S")
+print(my_new_tesla.engine_info())
+print(my_new_tesla.battery_info())
