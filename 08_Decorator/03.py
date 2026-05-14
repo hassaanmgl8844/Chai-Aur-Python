94372a0a4f5fe0b83655eda2f69bba58f13918fe
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-14 12:56:20 +0500

    Done Decorators Done!

---
+++ 08_Decorator/03.py
@@ -0,0 +1,18 @@
+import time
+
+def cache(func):
+    cache_value = {}
+    print(cache_value)
+    def wrapper(*args):
+        if args in cache_value:
+            return cache_value[args]
+        result = func(*args)
+        cache_value[args] = result
+        return result
+    return wrapper
+
+@cache
+def long_runn(a,b):
+    time.sleep(4)
+    return a + b
+print(long_runn(1,3))
