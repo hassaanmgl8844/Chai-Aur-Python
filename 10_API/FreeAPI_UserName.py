3ff7722e9a48b9bb4f3c631ea65330d1ddaa2f70
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-22 04:30:05 +0500

    API Data Fetching Learned

--- 10_API/FreeAPI_UserName.py
+++ 10_API/FreeAPI_UserName.py
@@ -2,24 +2,25 @@ import requests
 
 
 def fetch_random_user_api():
-    # url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
-    url = "https://api.freeapi.app/api/v1/public/randomjokes/joke/random"
+    url = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
     response = requests.get(url)
     data = response.json()
 
     if data["success"] and "data" in data:
         user_data = data["data"]
-        userdata = user_data["id"]
-        usercontent = user_data["content"]
-        return userdata,usercontent
+        author = user_data["author"]
+        authorcontent = user_data["content"]
+        return author,authorcontent
     else:
         raise Exception("Failed to Fetch data!")
 
 
 def main():
     try:
-        userdata, usercontent = fetch_random_user_api()
-        print(f"Id: {userdata} , Content: {usercontent}")
+        author, authorcontent = fetch_random_user_api()
+        print(
+            f"Author: {author} , Content: {authorcontent}"
+        )
     except Exception as e:
         print(str(e))
 
