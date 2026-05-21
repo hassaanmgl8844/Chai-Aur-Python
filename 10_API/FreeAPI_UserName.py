440d2a55e9ff0e7469c5c9956671cc8e0a4d1443
hassaanmgl8844 <hsnprog8844@gmail.com>
2026-05-21 11:19:39 +0500

    It's Done

---
+++ 10_API/FreeAPI_UserName.py
@@ -0,0 +1,10 @@
+import requests
+
+def fetch_random_user_api():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
